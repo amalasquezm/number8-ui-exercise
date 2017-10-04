c76a8f9fd33de83098830023bf5a35ba935ab9ba
Alejandro M <amalasquezm@gmail.com>
2017-10-04 11:14:12 -0500

    Updated component

--- angular-exercise-n8/src/app/custom-calendar/custom-calendar.component.ts
+++ angular-exercise-n8/src/app/custom-calendar/custom-calendar.component.ts
@@ -14,22 +14,24 @@ export class CustomCalendarComponent implements OnInit {
 
     startDate: Date;
     holidays: any;
-    monthsInvolved: number;
     startDateModel: any;
     months: Month[] = [];
+    monthNames = ["January", "February", "March", "April", "May", "June",
+                  "July", "August", "September", "October", "November", "December"];
     
     constructor() { }
 
     ngOnInit() {
     }
 
-    getMonthsInvolved(): number
-    {
-        let lastDayOfSeledtedMonth = new Date(this.startDate.getFullYear(), this.startDate.getMonth() + 1, 0);
-        let lastDaySelected = new Date();
+    getLastSelectedDate(): Date {
+        let lastDaySelected: Date = new Date();
         lastDaySelected.setDate(this.startDate.getDate() + parseInt(this.days));
+        return lastDaySelected;        
+    }
 
-        return lastDaySelected.getMonth() - this.startDate.getMonth() + 1;
+    getHolidays() {
+
     }
 
     isHoliday(date: Date): boolean {
@@ -37,28 +39,57 @@ export class CustomCalendarComponent implements OnInit {
     }
 
     isWeekend(date: Date): boolean {
-        return false; 
+        return date.getDay() == 0 || date.getDay() == 6; 
     }
 
     isLastDayOfMonth(date: Date): boolean {
-        return false;
+        let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+        return lastDay.getDate() == date.getDate();
+    }
+
+    isLastDayOfWeek(date: Date): boolean {
+        return date.getDay() == 6;
+    }
+
+    getPreviousSunday(date: Date): Date {
+        date.setDate(date.getDate() - date.getDay());
+        return date;
+    }
+
+    getPosteriorSaturday(date: Date): Date {
+        date.setDate(date.getDate() + 6 - date.getDay());
+        return date;
+    }
+
+    completeInvalidDays(start: Date, end: Date, week: Week) {
+      for (var d = start; d < end; d.setDate(d.getDate() + 1)) {
+          week.days.push(this.getInvalidDay(new Date(d)));
+      }
+    }
+
+    getInvalidDay(date: Date) : Day {
+        return { isDisabled: true, dayNumber: date.getDate(), isWeekend: false, isHoliday: false };
+    }
+
+    getValidDay(date: Date) : Day {
+        return { isDisabled: false, dayNumber: date.getDate(), isWeekend: this.isWeekend(date), isHoliday: this.isHoliday(date) };
+
     }
 
     onGenerateCalendarClick() {
         this.startDate = new Date(this.startDateString);
-        console.log(this.startDateString);
-        console.log(this.startDate);
+        this.generateCalendar(this.startDate, this.getLastSelectedDate());
     }
 
     generateCalendar(start: Date, end: Date) {
         console.log("start:" + start);
         console.log("end:" + end);
 
-        this.monthsInvolved = this.getMonthsInvolved();
         let lastDay = new Date();
 
         //month
         let month = new Month();
+        month.name = this.monthNames[start.getMonth()];
         month.year = start.getFullYear();
         month.weeks = []; 
 
@@ -72,26 +103,36 @@ export class CustomCalendarComponent implements OnInit {
             if(this.isLastDayOfMonth(d)) {
                 console.log("Last day:" + d);
                 //TODO add to week
+                week.days.push(this.getValidDay(d));
+                this.completeInvalidDays(d, this.getPosteriorSaturday(d), week);
                 month.weeks.push(week);
                 this.months.push(month);
 
                 //Create new month                
                 month = new Month();
+                month.name = this.monthNames[d.getMonth()];
                 month.year = d.getFullYear();  
                 month.weeks = [];
             }
 
             //check if last day of week
-            
+            else if(this.isLastDayOfWeek(d)) {
+                //TODO add to week
+                week.days.push(this.getValidDay(d));
+                month.weeks.push(week);
+                week = new Week();
+                week.days = [];
+            }
 
             else {
                 console.log("Valid day:" + d);
-                
+                week.days.push(this.getValidDay(d));
             }
-            //console.log(this.months)
+
+            lastDay = d;
         }
 
-        
+        this.completeInvalidDays(lastDay, this.getPosteriorSaturday(lastDay), week);
         month.weeks.push(week);
         this.months.push(month);
 
--- angular-exercise-n8/src/app/models/day.model.ts
+++ angular-exercise-n8/src/app/models/day.model.ts
@@ -1,5 +1,5 @@
 export class Day {
-    public day: Date = new Date();
+    public dayNumber: number;
     public isWeekend: boolean = false;
     public isHoliday: boolean = false;
     public isDisabled: boolean;
